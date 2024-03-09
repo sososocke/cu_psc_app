59996f48039564f89887630ed053a4891a853d01
Linda Heinrich <linda.heinrich@siemens.com>
2024-03-10 00:02:01 +0100

    implemented side menu, removed button ripple effect approach

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,47 +1,17 @@
 import { Component } from '@angular/core';
-import { IonButton } from '@ionic/angular';
+import { IonButton, IonMenu } from '@ionic/angular/standalone';
+import { MenuController } from '@ionic/angular/common';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonMenuButton } from '@ionic/angular/standalone';
+import { IonButtons } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonMenu, IonButtons, IonMenuButton],
 })
 export class HomePage {
-  constructor() {
-    const buttons = document.getElementsByTagName("button");
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener("click", createRipple);
-    }
-    console.log(buttons);
-    console.log("start");
-  }
-
-
-  say_hello(): void {
-    console.log("Hello :)");
-  }
-}
-
-function createRipple(event: any) {
-  console.log("ripple");
-  const button = event.currentTarget;
-  const circle = document.createElement("span");
-  const diameter = Math.max(button.clientWidth, button.clientHeight);
-  const radius = diameter / 2;
-
-  circle.style.width = circle.style.height = `${diameter}px`;
-  circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
-  circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
-  circle.classList.add("ripple");
-  const ripple = button.getElementsByClassName("ripple")[0];
-
-  if (ripple) {
-    ripple.remove();
-  }
-
-  button.appendChild(circle);
-
+  constructor() {}
 }
